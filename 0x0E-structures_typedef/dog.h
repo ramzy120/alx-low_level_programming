960d8c41ea9ece552aa2883137af7a03d095e979
ramzy120 <ramseyomrore@gmail.com>
2023-10-11 11:24:41 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,14 +2,14 @@
 #define DOG_H
 
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 	struct dog
 	{
 		char *name;
@@ -32,4 +32,4 @@
 	int _strlen(char *s);
 
 
-	#endif
+#endif
