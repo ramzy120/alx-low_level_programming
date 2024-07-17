9ff5c8a699603ff4e1cf585fef4dc6641aaf0f94
ramzy120 <ramseyomrore@gmail.com>
2024-07-17 21:30:32 +0100

    update

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return -1;
+    
+    for (size_t i = 0; i < size; i++)
+    {
+        if (array[i] == value)
+            return i;
+    }
+    
+    return -1;
+}
